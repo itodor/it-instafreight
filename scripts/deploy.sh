1efffda2868665d2321ca06c173a3912a0324d2f
itodor <ivan.todorovic17@gmail.com>
2022-07-02 10:35:59 +0200

    [#3] Playing with travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo "deploying magic..."
+set -xe
+
+if [ $TRAVIS_BRANCH == 'master' ] ; then
+#  eval "$(ssh-agent -s)"
+#  ssh-add ~/.ssh/id_rsa
+#
+#  rsync -a --exclude={'/node_modules','/src','/public'} client/ travis@<droplet ipaddress>:/home/<sudo user>/demo/client
+  echo "Deploying starts"
+else
+  echo "Not deploying, since the branch isn't master."
+fi
